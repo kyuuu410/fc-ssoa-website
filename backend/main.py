8e1430c0e558bdc376999af3f12d23823e096aab
LKK <dlrbrud0410@gmail.com>
2025-12-17 11:29:15 +0900

    Fix CORS for production

--- backend/main.py
+++ backend/main.py
@@ -15,6 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000"
+        "https://fc-ssoa-frontend.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
